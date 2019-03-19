69caa65f4802f78279c86d6171bab4defa747aff
silence <yangkemuyi@sina.com>
2019-03-19 18:53:58 +0800

    更新界面

--- LTGameSdkUI/src/main/java/com/gnetop/ltgameui/manager/LoginUIManager.java
+++ LTGameSdkUI/src/main/java/com/gnetop/ltgameui/manager/LoginUIManager.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.text.TextUtils;
 
 import com.gnetop.ltgamecommon.model.ResultData;
+import com.gnetop.ltgamegoogle.login.GooglePlayLoginManager;
+import com.gnetop.ltgamegoogle.login.OnGoogleSignOutListener;
 import com.gnetop.ltgameui.base.Constants;
 import com.gnetop.ltgameui.impl.OnReLoginInListener;
 import com.gnetop.ltgameui.util.PreferencesUtils;
@@ -37,6 +39,14 @@ public class LoginUIManager {
 
     /**
      * 登录进入
+     *
+     * @param activity       上下文
+     * @param mAgreementUrl  用户协议
+     * @param mPrivacyUrl    隐私协议
+     * @param googleClientID googleClientID
+     * @param LTAppID        乐推AppID
+     * @param LTAppKey       乐推AppKey
+     * @param mListener      登录接口
      */
     public void loginIn(Activity activity, String mAgreementUrl, String mPrivacyUrl, String googleClientID,
                         String LTAppID, String LTAppKey, OnReLoginInListener mListener) {
@@ -52,17 +62,30 @@ public class LoginUIManager {
 
     /**
      * 登出
+     *
+     * @param activity       上下文
+     * @param mAgreementUrl  用户协议
+     * @param mPrivacyUrl    隐私协议
+     * @param googleClientID googleClientID
+     * @param LTAppID        乐推AppID
+     * @param LTAppKey       乐推AppKey
      */
-    public void loginOut(Activity activity, String mAgreementUrl, String mPrivacyUrl, String googleClientID,
-                         String LTAppID, String LTAppKey) {
+    public void loginOut(final Activity activity, final String mAgreementUrl, final String mPrivacyUrl,
+                         final String googleClientID,
+                         final String LTAppID, final String LTAppKey) {
         PreferencesUtils.remove(activity, Constants.USER_LT_UID);
         PreferencesUtils.remove(activity, Constants.USER_LT_UID_TOKEN);
-        if (TextUtils.isEmpty(PreferencesUtils.getString(activity,
-                Constants.USER_LT_UID)) &&
-                TextUtils.isEmpty(PreferencesUtils.getString(activity,
-                        Constants.USER_LT_UID_TOKEN))) {
-            login(activity, mAgreementUrl, mPrivacyUrl, googleClientID, LTAppID, LTAppKey);
-        }
+        GooglePlayLoginManager.GoogleSingOut(activity, googleClientID, new OnGoogleSignOutListener() {
+            @Override
+            public void onSignOutSuccess() {
+                if (TextUtils.isEmpty(PreferencesUtils.getString(activity,
+                        Constants.USER_LT_UID)) &&
+                        TextUtils.isEmpty(PreferencesUtils.getString(activity,
+                                Constants.USER_LT_UID_TOKEN))) {
+                    login(activity, mAgreementUrl, mPrivacyUrl, googleClientID, LTAppID, LTAppKey);
+                }
+            }
+        });
     }
 
     /**
